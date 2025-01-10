64cf5c7215eb8595c5604873215f3a577c3b319e
Akmal Khair <76650559+AkmalKhair@users.noreply.github.com>
2025-01-10 14:47:32 +0700

    Update Main.php

--- src/AkmalKhair/CustomRankJoinMessage/Main.php
+++ src/AkmalKhair/CustomRankJoinMessage/Main.php
@@ -8,8 +8,7 @@ use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\event\player\PlayerQuitEvent;
 use pocketmine\plugin\PluginBase;
-use pocketmine\utils\TextFormat;
-use IvanCraft623\RankSystem\RankSystem; // Assuming RankSystem provides a RankManager class
+use IvanCraft623\RankSystem\RankSystem;
 use IvanCraft623\RankSystem\utils\Utils;
 use pocketmine\Server;
 
@@ -22,56 +21,29 @@ class Main extends PluginBase implements Listener {
         $this->saveDefaultConfig();
         $this->joinMessages = $this->getConfig()->get("join-messages", []);
         $this->leaveMessages = $this->getConfig()->get("leave-messages", []);
-
-        $this->getLogger()->info(TextFormat::GREEN . "CustomRankJoinMessage plugin enabled!");
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
-    }
-
-    public function onDisable(): void {
-        $this->getLogger()->info(TextFormat::RED . "CustomRankJoinMessage plugin disabled!");
     }
 
     public function onPlayerJoin(PlayerJoinEvent $event): void {
         $player = $event->getPlayer();
         $session = RankSystem::getInstance()->getSessionManager()->get($player->getName());
-    
-        // Set pesan default sementara (hanya untuk mencegah kosong)
         $event->setJoinMessage("");
-    
-        // Tunggu hingga sesi diinisialisasi
         $session->onInitialize(function() use ($player, $session) {
-            $rank = Utils::ranks2string($session->getRanks());
-            if (empty($rank)) {
-                $rank = ''; // Rank default jika rank kosong
-            }
-    
+            $rank = Utils::ranks2string($session->getRanks()) ?? "";
             $customMessage = $this->joinMessages[$rank] ?? "Welcome to the kingdom, {player}!";
             $customMessage = str_replace("{player}", $player->getName(), $customMessage);
-    
-            // Broadcast pesan ke seluruh pemain
-            Server::getInstance()->broadcastMessage(TextFormat::AQUA . $customMessage);
+            Server::getInstance()->broadcastMessage($customMessage);
         });
     }
-
     public function onPlayerQuit(PlayerQuitEvent $event): void {
         $player = $event->getPlayer();
         $session = RankSystem::getInstance()->getSessionManager()->get($player->getName());
-
-        // Set pesan default sementara (hanya untuk mencegah kosong)
         $event->setQuitMessage("");
-
-        // Tunggu hingga sesi diinisialisasi
         $session->onInitialize(function() use ($player, $session) {
-            $rank = Utils::ranks2string($session->getRanks());
-            if (empty($rank)) {
-                $rank = ''; // Rank default jika rank kosong
-            }
-
+            $rank = Utils::ranks2string($session->getRanks()) ?? "";
             $customMessage = $this->leaveMessages[$rank] ?? "Farewell, {player}. May your journey be safe.";
             $customMessage = str_replace("{player}", $player->getName(), $customMessage);
-    
-            // Broadcast pesan ke seluruh pemain
-            Server::getInstance()->broadcastMessage(TextFormat::AQUA . $customMessage);
+            Server::getInstance()->broadcastMessage($customMessage);
         });
     }
 }
